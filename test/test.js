d4eb3cd4941ae54c461c21e73bdab200c3600e1c
Danny Hieber <dwhieb@gmail.com>
2019-08-21 21:38:03 -0500

    add test data

--- test/test.js
+++ test/test.js
@@ -1,14 +1,22 @@
+const path         = require(`path`);
+const { readFile } = require(`fs`).promises;
+
 describe(`tags2dlx`, function() {
 
   let convert;
+  let text;
 
   beforeAll(async function() {
     ({ default: convert } = await import(`../src/index.js`));
+    text = await readFile(path.join(__dirname, `./test.txt`), `utf8`);
   });
 
-  // TODO: remove this test
-  it(`is a function`, function() {
-    expect(typeof convert).toBe(`function`);
+  it(`returns a DLx Text object`, function() {
+
+    const output = convert(text);
+
+    expect(typeof output).toBe(`object`);
+
   });
 
 });
