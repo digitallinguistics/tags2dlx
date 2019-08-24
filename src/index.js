f3c06e28bd9aa692473b57b352f7a6afd243cf25
Daniel W. Hieber <dwhieb@gmail.com>
2019-08-24 11:22:22 -0500

    NEW: tokenization
    closes #8
    closes #9
    closes #10
    closes #12
    closes #30

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
-/**
- * Default options for the tags2dlx function
- * @type {Object}
- */
-const defaultOptions = {
-  metadata: {},
-};
+// CONSTANTS
 
 /**
  * Default (empty) DLx Text object
@@ -16,20 +10,93 @@ const defaultText = {
 };
 
 /**
- * Convert a tagged text to DLx JSON format
- * @param  {String} [text=``]                The tagged text to convert
- * @param  {Object} [options=defaultOptions] An options object
- * @param  {Object} [options.metadata={}]    An object containing additional metadata to add to the text, such as title, etc.
- * @return {Object}                          Returns a DLx Text object
+ * Regular expression for finding one or more white spaces
+ * @type {RegExp}
  */
-function tags2dlx(text = ``, { metadata } = defaultOptions) {
+const whiteSpaceRegExp = /\s+/gu;
 
-  const json = { ...defaultText, ...Object(metadata) };
 
-  json.utterances = [];
+// METHODS
 
-  return json;
-
+/**
+ * Creates a function to parse a tagged word into the word token and its tag.
+ * @param  {String}   tagSeparator The tag separator to split on (sits between the word token and its tag)
+ * @return {Function}              Returns the parseTaggedWord function
+ */
+function createWordParser(tagSeparator) {
+  /**
+   * Parses a tagged word into the word token and its tag
+   * @param  {String} taggedWord The tagged word to parse
+   * @return {Object}            Returns an object with "tag" and "token" properties
+   */
+  return taggedWord => {
+    const [token, tag] = taggedWord.split(tagSeparator);
+    return { tag, token };
+  };
 }
 
-export default tags2dlx;
+// MAIN
+
+/**
+ * Convert a tagged text to DLx JSON format
+ * @param  {String} [text=``]                             The tagged text to convert
+ * @param  {Object} [options]                             An options object
+ * @param  {Object} [options.metadata={}]                 An object containing additional metadata to add to the Text, such as title, etc.
+ * @param  {String} [options.punctuation=`,`]             Punctuation to ignore. Tagged items consisting of one of these characters will be removed from the output.
+ * @param  {String} [options.tagSeparator=`_`]            The character(s) delimiting the word token from its tag
+ * @param  {String} [options.utteranceSeparators=`.!?"'`] A string containing all the characters to treat as utterance separators.
+ * @return {Object}                                       Returns a DLx Text object
+ */
+export default function tags2dlx(
+  text = ``,
+  {
+    metadata            = {},
+    punctuation         = `,`,
+    tagSeparator        = `_`,
+    utteranceSeparators = `.!?"'`,
+  } = {},
+) {
+
+  punctuation         = [...punctuation];         // eslint-disable-line no-param-reassign
+  utteranceSeparators = [...utteranceSeparators]; // eslint-disable-line no-param-reassign
+
+  const parseTaggedWord = createWordParser(tagSeparator);
+
+  const words = text
+  .split(whiteSpaceRegExp)
+  .map(parseTaggedWord);
+
+  // NB: To keep this code readable, don't chain this off the words array
+  const utterances = words.reduce((arr, word) => {
+
+    // if current word token is an utterance separator, start a new utterance
+    if (utteranceSeparators.includes(word.token)) {
+
+      arr.push({ words: [] });
+
+    // filter out punctuation words *after* tokenizing text into utterances
+    // in case the same character is included in both punctuation and utterance separators
+    } else if (punctuation.includes(word.token)) {
+
+      return arr;
+
+    // add current word token to current utterance
+    } else {
+
+      const lastUtterance = arr[arr.length - 1];
+      lastUtterance.words.push(word);
+
+    }
+
+    return arr;
+
+  }, [{ words: [] }])
+  .filter(u => u.words.length);
+
+  return {
+    ...defaultText,
+    ...Object(metadata),
+    utterances,
+  };
+
+}
--- test/test.js
+++ test/test.js
@@ -6,6 +6,8 @@ describe(`tags2dlx`, function() {
   let convert;
   let text;
 
+  const tinyText = `This_DEM is_COP a_DET sentence_N ._. Is_COP this_DEM a_DET question_N ?_?`;
+
   beforeAll(async function() {
     ({ default: convert } = await import(`../src/index.js`));
     text = await readFile(path.join(__dirname, `./test.txt`), `utf8`);
@@ -42,11 +44,44 @@ describe(`tags2dlx`, function() {
           },
         };
 
-        const output = convert(text, options);
+        const output = convert(tinyText, options);
 
-        expect(output.utterances.length).toBe(0);
+        expect(output.utterances.length).toBe(2);
 
       });
+
+    });
+
+    it(`punctuation`, function() {
+
+      const input       = `word_N -_- word_N`;
+      const punctuation = `-`;
+
+      const { utterances: [{ words }] } = convert(input, { punctuation });
+
+      expect(words.length).toBe(2);
+
+    });
+
+    it(`tag separator`, function() {
+
+      const input        = `home^N`;
+      const tagSeparator = `^`;
+
+      const { utterances: [{ words: [{ token }] }] } = convert(input, { tagSeparator });
+
+      expect(token).toBe(`home`);
+
+    });
+
+    it(`utterance separators`, function() {
+
+      const input               = `sentence_N ._. sentence_N ?_? sentence_N !_!`;
+      const utteranceSeparators = `.!?`;
+
+      const { utterances } = convert(input, { utteranceSeparators });
+
+      expect(utterances.length).toBe(3);
 
     });
 
@@ -60,6 +95,22 @@ describe(`tags2dlx`, function() {
 
       expect(typeof output.title).toBe(`object`);
       expect(Array.isArray(output.utterances)).toBe(true);
+
+    });
+
+    it(`tokenizes text into utterances`, function() {
+
+      const { utterances } = convert(tinyText);
+
+      expect(utterances.length).toBe(2);
+
+    });
+
+    it(`tokenizes utterances into words`, function() {
+
+      const { utterances: [{ words }] } = convert(tinyText);
+
+      expect(words.length).toBe(4); // eslint-disable-line no-magic-numbers
 
     });
 
